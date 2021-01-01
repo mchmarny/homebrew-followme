389b3aa7124998c8b287a39800149af3dca5f6c2
Mark Chmarny <mchmarny@users.noreply.github.com>
2021-01-01 06:27:22 -0800

    Update followme.rb

--- Formula/followme.rb
+++ Formula/followme.rb
@@ -2,7 +2,7 @@
 class Followme < Formula
   desc "Utility to monitor Twitter followers"
   homepage "https://thingz.io"
-  url "https://github.com/mchmarny/followme/releases/download/v0.3.1/followme"
+  url "https://github.com/mchmarny/followme/releases/download/v0.3.2/followme"
   sha256 "84eaa63eaab863efcfed6218c5a86778d51228ba00619b84aa5c1ca3157a0b17"
   bottle :unneeded
 
